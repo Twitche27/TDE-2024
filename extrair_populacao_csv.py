1595fc572662a7497c6958dc4e1aee8d30de45be
G. Simões <129446131+sugarfreecat@users.noreply.github.com>
2024-05-26 15:53:03 -0300

    Update extrair_populacao_csv.py
    não terminado

--- extrair_populacao_csv.py
+++ extrair_populacao_csv.py
@@ -1,36 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select
 from xpath import xpath_lista
 import time
 
-PATH = "C:\\Program Files (x86)\\chromedriver.exe"
-service = Service(PATH)
-options = Options()
-driver = webdriver.Chrome(service=service, options=options)
-
 class Populacao(object):
-
+    driver = None
     def __init__(self, link):
-        driver.get(link)
+        self.driver = webdriver.Chrome()
+        self.driver.get(link)
 
     def selecionar_variaveis(self, xpath):
-        WebDriverWait(driver, 20).until(EC.invisibility_of_element((By.CLASS_NAME, "loading-logo carregado")))
+        WebDriverWait(self.driver, 20).until(EC.invisibility_of_element((By.CLASS_NAME, "loading-logo carregado")))
         for x in xpath:
-            driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, f'{x}'))))
+            self.driver.execute_script("arguments[0].click();", WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, f'{x}'))))
 
     def selecionar_formato(self, xpath):
-        teste_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'{xpath}')))
+        teste_element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'{xpath}')))
         teste = Select(teste_element)
         teste.select_by_visible_text('CSV (BR)')
 
     def fazer_download(self, xpath):
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'{xpath}'))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'{xpath}'))).click()
 
 
 if __name__ == '__main__':
