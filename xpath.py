d73e04a749e6ac871178c3527213722592c8f0d7
G. Simões <129446131+sugarfreecat@users.noreply.github.com>
2024-05-26 22:00:05 -0300

    Delete xpath.py

--- xpath.py
+++
@@ -1,9 +0,0 @@
-xpath_lista = ['//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[2]/div/div/div/button', 
-              '//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[3]/div/div/div/button',
-              '//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[4]/div/div/div/button',
-              '//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[5]/div/div/div/button',
-              '//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[6]/div/div/div/button',
-              '//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[7]/div/div/div/button',
-              '//*[@id="panel-P-collapse"]/div[3]/div/div[2]/div/div/div/div/div[8]/div/div/div/button',
-              '//*[@id="arvore-245e-1"]/div/div/div/button', '//*[@id="arvore-325e-1"]/div/div/div/button',
-              '//*[@id="botao-downloads"]']
